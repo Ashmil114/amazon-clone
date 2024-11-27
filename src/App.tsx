fa804fac579afda195a9b6cf80ee22d765da0b8a
Ashmil <ashmilk114@gmail.com>
2024-11-27 23:06:31 +0530

    Home Banner Completed

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ export default function App() {
     <>
       <Navbar />
       <Home />
+      <div className="h-[1000px]">hi</div>
     </>
   );
 }
--- src/sections/Home/Home.tsx
+++ src/sections/Home/Home.tsx
@@ -2,12 +2,25 @@ import "./Home.css";
 function Home() {
   return (
     <div>
-      <div className="max-h-[230px] overflow-visible relative   home-banner ">
-        <img
-          src="https://m.media-amazon.com/images/I/71zpBcCjKPL._SX3000_.jpg"
-          alt=""
-          className="w-full "
-        />
+      {/* Banner Section */}
+      <div className=" max-h-[230px]   h-[300px] overflow-visible">
+        <div className="home-banner"></div>
+      </div>
+
+      {/* Small Description box */}
+      <div className="px-[10px]">
+        <div className="mt-[20px] px-[10px]">
+          <div className="bg-[#ffff] px-[14px] py-[10px] flex items-center justify-center">
+            <p className="font-ember text-[14px] leading-[20px]">
+              You are on amazon.com. You can also shop on Amazon India for
+              millions of products with fast local delivery.
+              <a href="" className="text-[#007185]">
+                {" "}
+                Click here to go to amazon.in
+              </a>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
